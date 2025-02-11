67604bd62e6fd8624553990dd41b7f0aa7160183
yaming <69099753+yaming8002@users.noreply.github.com>
2025-02-11 13:05:48 +0800

    no message

--- w_trans/service/update_steel_by_month.py
+++ w_trans/service/update_steel_by_month.py
@@ -26,6 +26,7 @@ def update_steel_by_month(year, month, first_day_of_month, last_day_of_month):
         & ~Q(translog__constn_site__code__in=["F001", "F002", "F003"])
         & ~( Q(remark__contains="#") & Q(translog__transaction_type='IN'))
         & Q(is_rollback=False)
+        & Q(is_rent=False)
     )
 
     update_list = (
