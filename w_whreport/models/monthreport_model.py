766af1edb67f6fd4649c80dbfab7c74dcfac52a3
yaming <69099753+yaming8002@users.noreply.github.com>
2025-03-10 18:40:16 +0800

    結案的不進行計算

--- w_whreport/models/monthreport_model.py
+++ w_whreport/models/monthreport_model.py
@@ -147,7 +147,8 @@ class MonthReport(MonthData):
     def update_column_value_by_before(cls, site: SiteInfo,year:int,month:int, is_add: bool, column: str, value: Decimal):
 
         now = cls.get_current_by_site(site,year, month)
-
+        if now.is_done:
+            return
         update_value = Decimal(getattr(now, column, 0))
         update_value += value if is_add else -value
         setattr(now,column,update_value)
